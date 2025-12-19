ade37ce920855e02087365fb9c0bbbf8223f9083
Xavier Tondelier <xtondelier@gmail.com>
2025-12-19 20:35:47 +0000

    Add basic JS to make an image carousel
    Also add 13 example images to use.

---
+++ src/image-carousel.js
@@ -0,0 +1,20 @@
+function createImageCarousel(images) {
+  const pictureDiv = document.createElement("div");
+  pictureDiv.classList.add("image-carousel");
+
+  const allImagesDiv = document.createElement("div");
+  allImagesDiv.classList.add("all-images");
+
+  for (const imageSrc of images) {
+    const image = document.createElement("img");
+    image.src = imageSrc;
+    image.alt = "item in image carousel";
+
+    allImagesDiv.appendChild(image);
+  }
+
+  pictureDiv.appendChild(allImagesDiv);
+  return pictureDiv;
+}
+
+export default createImageCarousel;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-import "./style.css";
+import "./image-carousel-style.css";
+import createImageCarousel from "./image-carousel.js";
+
+import image1 from "./images/image1.jpg";
+import image2 from "./images/image1.jpg";
+import image3 from "./images/image1.jpg";
+import image4 from "./images/image1.jpg";
+import image5 from "./images/image1.jpg";
+import image6 from "./images/image1.jpg";
+import image7 from "./images/image1.jpg";
+import image8 from "./images/image1.jpg";
+import image9 from "./images/image1.jpg";
+import image10 from "./images/image1.jpg";
+import image11 from "./images/image1.jpg";
+import image12 from "./images/image1.jpg";
+import image13 from "./images/image1.jpg";
+
+const images = [image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12, image13]
+
+const imageCarousel = createImageCarousel(images);
+
+document.body.appendChild(imageCarousel);
+
+
